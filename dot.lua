6ca9b0650770d02c30414f55d2461e7fd39551a6
Brettell <thomas.a.brettell@gmail.com>
2025-03-03 21:37:11 +1100

    remove vectors being tables

--- dot.lua
+++ dot.lua
@@ -1,32 +1,40 @@
-local vector = require "vector"
 local Object = require "classic"
 
 Dot = Object:extend()
 Object.r = 5
 
 function Dot:new(x, y)
-	self.position = vector(x, y)
-	self.prev_position = vector(x, y)
-	self.acceleration = vector()
+	self.position_x = x
+	self.position_y = y
+
+	self.prev_position_x = x
+	self.prev_position_y = y
+	
+	self.acceleration_x = 0
+	self.acceleration_y = 0
 end
 
 function Dot:updatePosition(dt)
-	local velocity = self.position - self.prev_position
+	local velocity_x = self.position_x - self.prev_position_x
+	local velocity_y = self.position_y - self.prev_position_y
 
-	self.prev_position = self.position
+	self.prev_position_x = self.position_x
+	self.prev_position_y = self.position_y
 
-	self.position = self.position + velocity + self.acceleration * dt * dt
+	self.position_x = self.position_x + velocity_x + self.acceleration_x * dt * dt
+	self.position_y = self.position_y + velocity_y + self.acceleration_y * dt * dt
 
-	self.acceleration.x = 0
-	self.acceleration.y = 0
+	self.acceleration_x = 0
+	self.acceleration_y = 0
 end
 
-function Dot:accelerate(acc)
-	self.acceleration = self.acceleration + acc
+function Dot:accelerate(acc_x, acc_y)
+	self.acceleration_x = self.acceleration_x + acc_x
+	self.acceleration_y = self.acceleration_y + acc_y
 end
 
 function Dot:draw()
-	love.graphics.circle("fill", self.position.x, self.position.y, self.r)
+	love.graphics.circle("fill", self.position_x, self.position_y, self.r)
 end
 
 return Dot
--- main.lua
+++ main.lua
@@ -1,19 +1,19 @@
-local vector = require "vector"
 local Dot = require "dot"
+local vector = require "hump.vector-light"
 
 local fps
 local dots = {}
 local window_width = love.graphics:getWidth()
 local window_height = love.graphics:getHeight()
 local sub_steps = 8
-local gravity = vector(0, 1000)
-local contraint_position = vector(window_width/2, window_height/2)
+local gravity = {x=0, y=1000}
+local contraint_position = {x=window_width/2, y=window_height/2}
 local contraint_radius = window_height / 2 - 50
 local interval = 0
 
 local function applyGravity()
 	for i, dot in ipairs(dots) do
-		dot:accelerate(gravity)
+		dot:accelerate(gravity.x, gravity.y)
 	end
 end
 
@@ -25,12 +25,17 @@ end
 
 local function applyConstraint()
 	for i, dot in ipairs(dots) do
-		local to_obj = dot.position - contraint_position
-		local dist = to_obj:len()
+		local to_obj_x = dot.position_x - contraint_position.x
+		local to_obj_y = dot.position_y - contraint_position.y
+
+		local dist = vector.len(to_obj_x, to_obj_y)
 		
 		if dist > contraint_radius - dot.r then
-			local n = to_obj / dist
-			dot.position = contraint_position + n * (contraint_radius - dot.r)
+			local n_x = to_obj_x / dist
+			local n_y = to_obj_y / dist
+
+			dot.position_x = contraint_position.x + n_x * (contraint_radius - dot.r)
+			dot.position_y = contraint_position.y + n_y * (contraint_radius - dot.r)
 		end
 	end
 end
@@ -42,15 +47,20 @@ local function solveCollisions()
 		for k = i+1, #dots do
 			local dot_2 = dots[k]
 
-            local collision_axis = dot_1.position - dot_2.position
-            local dist = collision_axis:len()
+			local collision_axis_x, collision_axis_y = vector.sub(dot_1.position_x, dot_1.position_y, dot_2.position_x, dot_2.position_y)
+
+			local dist = vector.len(collision_axis_x, collision_axis_y)
 			local min_dist = dot_1.r + dot_2.r
 
             if dist < min_dist then
-				local n = collision_axis / dist
+				local n_x, n_y = vector.div(dist, collision_axis_x, collision_axis_y)
 				local delta = min_dist - dist
-				dot_1.position = dot_1.position + 0.5 * delta * n
-				dot_2.position = dot_2.position - 0.5 * delta * n
+				
+				dot_1.position_x = dot_1.position_x + 0.5 * delta * n_x
+				dot_1.position_y = dot_1.position_y + 0.5 * delta * n_y
+
+				dot_2.position_x = dot_2.position_x - 0.5 * delta * n_x
+				dot_2.position_y = dot_2.position_y - 0.5 * delta * n_y
 			end
         end
     end
@@ -78,11 +88,12 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.print(fps, 5, 5)
-    love.graphics.print("Ojects: " .. #dots, 5, 20)
-
     for i, dot in ipairs(dots) do
 		dot:draw()
 	end
+
+	love.graphics.print(fps, 5, 5)
+    love.graphics.print("Ojects: " .. #dots, 5, 20)
+	love.graphics.print(math.floor(collectgarbage("count")), 5, 35)
 end
 
